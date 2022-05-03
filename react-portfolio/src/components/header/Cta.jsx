d55b788ade9137859d77035597bd6bb294c93f3f
jordydp <jdepril@outlook.com>
2022-05-03 09:52:10 +0200

    Use React Icons + Header Component & styles
    Adding styles to header.css and in Index.css + creating Header.jsx

---
+++ react-portfolio/src/components/header/Cta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cta = () => {
+  return (
+    <div className='cta'>
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default Cta
--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './header.css'
-
+import Cta from './Cta'
+import fotomezelf from '../../assets/fotomezelf.jpg'
+import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello i'm</h5>
+        <h1>Jordy De Pril</h1>
+        <h5 className="text-light">Fullstack Developer student.</h5>
+        <Cta />
+        <HeaderSocials />
+        <div className="me">
+          <img src={fotomezelf} alt="me" />
+        </div>
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
