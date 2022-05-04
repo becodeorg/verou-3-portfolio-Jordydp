74d4ccdb81be4497bfd52732409c1fe10c713141
jordydp <jdepril@outlook.com>
2022-05-04 10:33:54 +0200

    Adding Projects to site and style

--- react-portfolio/src/components/about/About.jsx
+++ react-portfolio/src/components/about/About.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import './about.css'
-import fotomezelf from '../../assets/fotomezelf.jpg'
-import {FaAward} from 'react-icons/fa'
-import {HiOutlineOfficeBuilding} from 'react-icons/hi'
+import React from "react";
+import "./about.css";
+import fotomezelf from "../../assets/fotomezelf.jpg";
+import { FaAward } from "react-icons/fa";
+import { HiOutlineOfficeBuilding } from "react-icons/hi";
 const About = () => {
-  return (
-    <section id='about'>
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
+    return (
+        <section id="about">
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
 
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={fotomezelf} alt="About Me" />
-          </div>
-        </div>
-        <div className="about__content">
-          <div className="about__cards">
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={fotomezelf} alt="About Me" />
+                    </div>
+                </div>
+                <div className="about__content">
+                    {/* <div className="about__cards">
           <article className="about__card">
             <FaAward className='about__icon' />
             <h5>Experience</h5>
@@ -34,14 +34,43 @@ const About = () => {
             <h5>Experience</h5>
             <small>+ lorem</small>
           </article>
-          </div>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque repudiandae soluta vitae quis fuga accusamus ratione enim optio similique maiores. Iste molestias repellat magnam incidunt explicabo reprehenderit ullam similique sapiente.
-          </p>
-          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
-        </div>
-      </div>
-    </section>
-  )
-}
+          </div> */}
+          {/* TODO:Text highlighters on keywords */}
+                    <p>
+                        I was born in Wetteren on 10/01/1992 and grew up in
+                        Lede. I graduated in 2010 in Accounting-Informatics.
+                        This was my first contact with programming. I have been
+                        working as a funeral advisor for 4 years now. I describe
+                        myself as a calm, flexible and responsible person who
+                        attaches great importance to supporting and helping
+                        people. Someone who is happy when the people around me
+                        are happy. This may explain why I have been working in
+                        healthcare for 11 years. <br /><br /> During the Covid pandemic, I
+                        consulted the Dela webdeveloper several times because of
+                        a problem that arose at the time. We went to search the
+                        most suitable solution together. Because of this I
+                        rediscovered the passion of problem solving. This in
+                        turn brought me to programming. What started with a
+                        search on Google became an activity in the little free
+                        time I have. Watching Youtube tutorials, following
+                        various developers on Instagram , buying and reading
+                        books, a Udemy course and the sololearn app. What also
+                        appeals to me is that I can once again help people build
+                        their future. The challenge is to find out the wishes of
+                        the customer and then translate them alone or together
+                        with a team into the design of their website. The
+                        constant innovation and always being kept on my toes is
+                        something that really speaks to me. This is the reason
+                        why I have stopped my job as a funeral advisor and want
+                        to fully focus on becoming a web developer.
+                    </p>
+                    <a href="#contact" className="btn btn-primary">
+                        Let's Talk
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default About
+export default About;
--- react-portfolio/src/components/portfolio/Portfolio.jsx
+++ react-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,77 @@
 import React from 'react'
 import './portfolio.css'
+import project1 from '../../assets/project1.jpg'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Projects i learned a lot from</h2>
+
+      <div className="div container project__container">
+        {/* TODO:Refactor to project component */}
+        {/* project 1 */}
+        <article className="article project__item">
+          <div className="project__item-image">
+            <img src={project1} alt="" />
+          </div>
+          <h3>Project Title</h3>
+          <a href="" className='btn'target='_blank'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </article>
+          {/* end of project 1 */}
+        {/* project 2 */}
+        <article className="article project__item">
+          <div className="project__item-image">
+            <img src={project1} alt="" />
+          </div>
+          <h3>Project Title</h3>
+          <a href="" className='btn'target='_blank'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </article>
+          {/* end of project 2 */}
+        {/* project 3 */}
+        <article className="article project__item">
+          <div className="project__item-image">
+            <img src={project1} alt="" />
+          </div>
+          <h3>Project Title</h3>
+          <a href="" className='btn'target='_blank'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </article>
+          {/* end of project 3 */}
+        {/* project 4 */}
+        <article className="article project__item">
+          <div className="project__item-image">
+            <img src={project1} alt="" />
+          </div>
+          <h3>Project Title</h3>
+          <a href="" className='btn'target='_blank'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </article>
+          {/* end of project 4 */}
+        {/* project 5 */}
+        <article className="article project__item">
+          <div className="project__item-image">
+            <img src={project1} alt="" />
+          </div>
+          <h3>Project Title</h3>
+          <a href="" className='btn'target='_blank'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </article>
+          {/* end of project 5 */}
+        {/* project 6 */}
+        <article className="article project__item">
+          <div className="project__item-image">
+            <img src={project1} alt="" />
+          </div>
+          <h3>Project Title</h3>
+          <a href="" className='btn'target='_blank'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </article>
+          {/* end of project 6 */}
+      </div>
+    </section>
   )
 }
 
