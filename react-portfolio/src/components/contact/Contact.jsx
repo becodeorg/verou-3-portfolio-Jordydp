0b735de471ba65ae5c8c8a743ab15928dbec0444
jordydp <jdepril@outlook.com>
2022-05-05 14:44:01 +0200

    Contact form + layout Contact component

--- react-portfolio/src/components/contact/Contact.jsx
+++ react-portfolio/src/components/contact/Contact.jsx
@@ -12,25 +12,30 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
         <article className='contact__option'>
-          <MdOutlineEmail />
+          <MdOutlineEmail className='contact__option-icon' />
           <h4>Email</h4>
           <h5>jdepril@outlook.com</h5>
           <a href="mailto:jdepril@outlook.com">Send a Message</a>
         </article>
         <article className='contact__option'>
-          <MdOutlineEmail />
+          <MdOutlineEmail className='contact__option-icon'/>
           <h4>Messenger</h4>
           <h5>jordy De Pril</h5>
           <a href="https://m.me/jordy.depril.9">Send a Message</a>
         </article>
         <article className='contact__option'>
-          <RiWhatsappLine />
+          <RiWhatsappLine className='contact__option-icon' />
           <h4>WhatsApp</h4>
           <h5>+32492067756</h5>
           <a href="https://api.whatsapp.com/send?phone+320492067756" target='_blank'>Send a Message</a>
         </article>
         </div>
-        <form action=""></form>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" placeholder='Enter Your Message Here' rows="7" required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
