c9d669b6ddf28a7519075c056c7b8adfc28c214a
jordydp <jdepril@outlook.com>
2022-05-05 11:05:07 +0200

    Create 3 contact ways: Messenger, WhatsApp & email

--- react-portfolio/src/components/contact/Contact.jsx
+++ react-portfolio/src/components/contact/Contact.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {RiWhatsappLine} from 'react-icons/ri'
 
 const Contact = () => {
   return (
     <section id='contact'>
-      Contact
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+        <article className='contact__option'>
+          <MdOutlineEmail />
+          <h4>Email</h4>
+          <h5>jdepril@outlook.com</h5>
+          <a href="mailto:jdepril@outlook.com">Send a Message</a>
+        </article>
+        <article className='contact__option'>
+          <MdOutlineEmail />
+          <h4>Messenger</h4>
+          <h5>jordy De Pril</h5>
+          <a href="https://m.me/jordy.depril.9">Send a Message</a>
+        </article>
+        <article className='contact__option'>
+          <RiWhatsappLine />
+          <h4>WhatsApp</h4>
+          <h5>+32492067756</h5>
+          <a href="https://api.whatsapp.com/send?phone+320492067756" target='_blank'>Send a Message</a>
+        </article>
+        </div>
+        <form action=""></form>
+      </div>
     </section>
   )
 }
